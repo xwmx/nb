8cc82aa587737c9dafc3815d063217d3f7819a01
William Melody <hi@williammelody.com>
2016-01-25 17:22:07 -0800

    Add `_compare()` function to test_helper.bash.

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -15,3 +15,22 @@ setup() {
   export NOTES_DIR="${BATS_TEST_DIRNAME}/tmp/.notes"
   export NOTESRC_PATH="${BATS_TEST_DIRNAME}/tmp/.notesrc"
 }
+
+###############################################################################
+# Helpers
+###############################################################################
+
+# _compare()
+#
+# Usage:
+#   _compare <expected> <actual>
+#
+# Description:
+#   Compare the content of a variable against an expected value. When used
+#   within a `@test` block the output is only displayed when the test fails.
+_compare() {
+  local _expected="${1:-}"
+  local _actual="${2:-}"
+  printf "expected:\n%s\n" "${_expected}"
+  printf "actual:\n%s\n" "${_actual}"
+}
