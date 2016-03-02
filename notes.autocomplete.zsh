5fddfa87bb57d77406d94637ceef3230509397ef
William Melody <hi@williammelody.com>
2016-03-01 17:53:53 -0800

    Add notes.autocomplete.zsh.

---
+++ notes.autocomplete.zsh
@@ -0,0 +1,30 @@
+_notes_subcommands() {
+  local _commands
+  _commands=($(notes commands))
+  local _notebooks
+  _notebooks=($(notes notebooks --names))
+  local _completions
+  _completions=(${_commands[@]})
+
+  # Construct <notebook>:<subcommand> completions.
+  for __notebook in "${_notebooks[@]}"
+  do
+    for __command in "${_commands[@]}"
+    do
+      if [[ -n "${__notebook:-}" ]] && [[ -n "${__command}" ]]
+      then
+        _completions+=("${__notebook}:${__command}")
+      fi
+    done
+  done
+
+  if [[ "${?}" -eq 0 ]]
+  then
+    compadd -- "${_completions[@]}"
+    return 0
+  else
+    return 1
+  fi
+}
+
+compdef _notes_subcommands notes
