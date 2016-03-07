49e70733cd07bb30754c1bf1f7b1fb88edca869c
William Melody <hi@williammelody.com>
2016-03-06 21:30:47 -0800

    Add notes.autocomplete.bash.

---
+++ notes.autocomplete.bash
@@ -0,0 +1,27 @@
+_notes_subcommands() {
+  local _commands
+  _commands=($(notes commands))
+  local _notebooks
+  _notebooks=($(notes notebooks --names --no-color))
+  local _completions
+  _completions=(${_commands[@]})
+
+  local _current="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=()
+
+  # Construct <notebook>:<subcommand> completions.
+  for __notebook in "${_notebooks[@]}"
+  do
+    for __command in "${_commands[@]}"
+    do
+      if [[ -n "${__notebook:-}" ]] && [[ -n "${__command}" ]]
+      then
+        _completions+=("${__notebook}:${__command}")
+      fi
+    done
+  done
+
+  COMPREPLY=($(compgen -W "${_completions[*]}" -- ${_current}))
+}
+
+complete -F _notes_subcommands notes
