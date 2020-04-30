9f3f84332a3fb80d48c6a56674f03adb9e9088ca
William Melody <hi@williammelody.com>
2020-04-30 12:42:52 -0700

    Add custom completion caching for zsh.

--- etc/notes-completion.zsh
+++ etc/notes-completion.zsh
@@ -1,66 +1,91 @@
 #compdef notes
 
 _notes_subcommands() {
-  local _commands
-  _commands=($(notes commands))
-  local _notebooks
-  _notebooks=($(notes notebooks --names --no-color --unarchived))
-  local _completions=()
+  _cache_completions() {
+    local _cache_path="${1:-}"
 
-  local _cache_path="${HOME}/.notes-completion-cache"
-  local _commands_match=0
-  local _counter=0
-  local _notebooks_match=0
+    local _commands
+    _commands=($(notes commands))
+
+    local _notebooks
+    _notebooks=($(notes notebooks --names --no-color --unarchived))
+
+    local _completions=()
+    _completions=(${_commands[@]})
+
+    local _commands_cached=
+    local _notebooks_cached=
+
+    if [[ -e "${_cache_path}" ]]
+    then
+      local _counter=0
+
+      while IFS= read -r __line
+      do
+        _counter=$((_counter+1))
+
+        if [[ "${_counter}" == 1 ]]
+        then
+          _commands_cached="${__line}"
+        elif [[ "${_counter}" == 2 ]]
+        then
+          _notebooks_cached="${__line}"
+        else
+          break
+        fi
+      done < "${_cache_path}"
+    fi
+
+    if [[ "${_commands_cached}"  != "${_commands[*]:-}"   ]] ||
+       [[ "${_notebooks_cached}" != "${_notebooks[*]:-}"  ]]
+    then
+      # Construct <notebook>:<subcommand> completions.
+      for __notebook in "${_notebooks[@]}"
+      do
+        for __command in "${_commands[@]}"
+        do
+          if [[ -n "${__notebook:-}" ]] && [[ -n "${__command}" ]]
+          then
+            _completions+=("${__notebook}:${__command}")
+          fi
+        done
+      done
+
+      printf "" > "${_cache_path}"
+      printf "%s\\n" "${_commands[*]}"    >> "${_cache_path}"
+      printf "%s\\n" "${_notebooks[*]}"   >> "${_cache_path}"
+      printf "%s\\n" "${_completions[@]}" >> "${_cache_path}"
+    fi
+  }
+
+  local _cache_path="${HOME}/.notes-completion-cache-zsh"
+  local _completions_cached=()
+
+  if [[ ! -e "${_cache_path}" ]]
+  then
+    _cache_completions "${_cache_path}"
+  fi
 
   if [[ -e "${_cache_path}" ]]
   then
+    local _counter=0
+
     while IFS= read -r __line
     do
       _counter=$((_counter+1))
 
-      if [[ "${_counter}" == 1 ]]
+      if [[ "${_counter}" -gt 2 ]]
       then
-        [[ "${__line}" == "${_commands[*]}" ]] &&
-          _commands_match=1
-      elif [[ "${_counter}" == 2 ]]
-      then
-        [[ "${__line}" == "${_notebooks[*]}" ]] &&
-          _notebooks_match=1
-      else
-        _completions+=("${__line}")
+        _completions_cached+=("${__line}")
       fi
     done < "${_cache_path}"
-  fi
 
-  if [[ ! -e "${_cache_path}" ]]  ||
-     ! ((_commands_match))        ||
-     ! ((_notebooks_match))       ||
-     [[ -z "${_completions[*]}" ]]
-  then
-
-    _completions=(${_commands[@]})
-
-    # Construct <notebook>:<subcommand> completions.
-    for __notebook in "${_notebooks[@]}"
-    do
-      for __command in "${_commands[@]}"
-      do
-        if [[ -n "${__notebook:-}" ]] && [[ -n "${__command}" ]]
-        then
-          _completions+=("${__notebook}:${__command}")
-        fi
-      done
-    done
-
-    printf "" > "${_cache_path}"
-    printf "%s\\n" "${_commands[*]}" >> "${_cache_path}"
-    printf "%s\\n" "${_notebooks[*]}" >> "${_cache_path}"
-    printf "%s\\n" "${_completions[@]}" >> "${_cache_path}"
+    (_cache_completions "${_cache_path}" &)
   fi
 
   if [[ "${?}" -eq 0 ]]
   then
-    compadd -- "${_completions[@]}"
+    compadd -- "${_completions_cached[@]}"
     return 0
   else
     return 1
