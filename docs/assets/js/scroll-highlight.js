11beaf334d48ece1047dd69591d967798f3cfef5
William Melody <hi@williammelody.com>
2020-09-25 21:39:50 -0700

    Improve scroll TOC behavior.

--- docs/assets/js/scroll-highlight.js
+++ docs/assets/js/scroll-highlight.js
@@ -1,7 +1,7 @@
 $(function() {
   $(window).scroll(function() {
     $("h1, h2, h3").each(function() {
-      if ($(window).scrollTop() >= $(this).offset().top) {
+      if ($(window).scrollTop() >= $(this).offset().top - 200) {
         var id = $(this).attr('id');
         $('.nav-list a').removeClass('active');
         $('.nav-list a[href="#' + id + '"]').addClass('active');
