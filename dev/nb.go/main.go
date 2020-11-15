93fe3cfddcf87364e800805b93bfe21d9938646d
William Melody <hi@williammelody.com>
2020-11-15 14:45:48 -0800

    Load $NB_DIR in nb.go.

--- dev/nb.go/main.go
+++ dev/nb.go/main.go
@@ -5,10 +5,46 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"runtime"
 	"syscall"
 )
 
+type configuration struct {
+	NBDir string
+}
+
+func (config *configuration) load() error {
+	dir := os.Getenv("NB_DIR")
+
+	if dir == "" {
+		if runtime.GOOS == "windows" {
+			dir = os.Getenv("APPDATA")
+
+			if dir == "" {
+				dir = filepath.Join(os.Getenv("USERPROFILE"), "Application Data", "nb")
+			}
+
+			dir = filepath.Join(dir, "nb")
+		} else {
+			dir = filepath.Join(os.Getenv("HOME"), ".nb")
+		}
+	}
+
+	config.NBDir = dir
+
+	return nil
+}
+
 func runScript() int {
+	var config configuration
+
+	err := config.load()
+	if err != nil {
+		log.Println(err)
+		return 1
+	}
+
 	binary, lookErr := exec.LookPath("nb")
 	if lookErr != nil {
 		log.Println(lookErr)
