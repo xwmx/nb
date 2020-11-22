ff5c5fe6c2c1c4917eeb2aa23fd3e184d0e65a06
William Melody <hi@williammelody.com>
2020-11-22 14:15:25 -0800

    Add header to main.go

--- dev/nb.go/main.go
+++ dev/nb.go/main.go
@@ -1,3 +1,33 @@
+// __          _
+// \ \   _ __ | |__       __ _  ___
+//  \ \ | '_ \| '_ \     / _` |/ _ \
+//  / / | | | | |_) | _ | (_| | (_) |
+// /_/  |_| |_|_.__/ (_) \__, |\___/
+//                       |___/
+//
+// [nb.go] An implementation of `nb` in go.
+//
+// ❯ https://github.com/xwmx/nb
+// ❯ https://xwmx.github.io/nb
+//
+// Copyright (c) 2020-present William Melody ┯ hi@williammelody.com
+//                                           ┕ https://www.williammelody.com
+//
+// AGPLv3
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package main
 
 // revive:disable:error-strings Errors should match existing `nb` formatting.
