d71c12b969cff160e5deb7fe4281af2af14bff22
William Melody <hi@williammelody.com>
2022-07-08 19:42:46 -0700

    Add `package.sh` for basher.

---
+++ package.sh
@@ -0,0 +1,11 @@
+###############################################################################
+# package.sh
+#
+# Configuration for basher, a package manager for shell scripts.
+#
+# https://github.com/basherpm/basher
+###############################################################################
+
+export BINS=nb
+export BASH_COMPLETIONS=etc/nb-completion.bash
+export ZSH_COMPLETIONS=etc/nb-completion.zsh
